c4b3b1971100d48ca8f0d8e265b0bf37993abb8a
ashokguna18 <53898384+ashokguna18@users.noreply.github.com>
2019-08-22 19:54:39 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="First Name"></asp:Label>
+            <asp:TextBox ID="TxtFN" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Last Name"></asp:Label>
+            <asp:TextBox ID="TxtLN" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Company"></asp:Label>
+            <asp:TextBox ID="TxtCompany" runat="server"></asp:TextBox><br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add" />
+        </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private DataTable dt;
+    private DataRow dr;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            dt = new DataTable();
+
+            dt.Columns.Add("FirstName");
+            dt.Columns.Add("LastName");
+            dt.Columns.Add("CompanyName");
+
+            dr = dt.NewRow();
+            dr[0] = "ramesh";
+            dr[1] = "ramanaiya";
+            dr[2] = "HW";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = "Ashok";
+            dr[1] = "";
+            dr[2] = "HW";
+            dt.Rows.Add(dr);
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            ViewState["tempData"] = dt;
+        }
+        else
+        {
+            dt = ViewState["tempData"] as DataTable;
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        dt = ViewState["tempData"] as DataTable;
+        dr = dt.NewRow();
+        dr[0] = TxtFN.Text;
+        dr[1] = TxtLN.Text;
+        dr[2] = TxtCompany.Text;
+        dt.Rows.Add(dr);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+        TxtFN.Text = "";
+        TxtLN.Text = "";
+        TxtCompany.Text = "";
+    }
+}
